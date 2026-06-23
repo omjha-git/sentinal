f18197a2f4c23235f51cbfb69931a590b308b83c
omjha <youremail@example.com>
2026-06-23 22:18:42 +0530

    Remove localhost API URLs

--- client/src/api/files.js
+++ client/src/api/files.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000";
+const API = import.meta.env.VITE_API_URL;
 
 export const getProjectFiles = async (projectId) => {
   const res = await axios.get(`${API}/api/projects/${projectId}/files`);
